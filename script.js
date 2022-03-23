88f0c9a2bfc980a931ca6e34226b95647407932f
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-03-23 16:23:42 -0700

    Update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ Promise.all([
 
 // Start webcam access
 function startVideo() {
-	navigator.mediaDevices.getUserMedia(
+	navigator.getUserMedia(
 		{ video: {} },
 		(stream) => (video.srcObject = stream),
 		(err) => console.error(err),
